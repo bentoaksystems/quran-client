f2ea915208f4d68984f59a217e8226f44c1f4728
305-2 <saresalehi@yaoo.com>
2017-06-20 12:38:55 +0430

    commit to pull from khatm branch

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -10,7 +10,7 @@ export class HttpService{
   // serverAddress: string = isDevMode()?'http://192.168.1.10:3000/api':
   //    'https://quran-together.herokuapp.com/api';
 
-  serverAddress: string = 'http://192.168.1.10:3000/api';
+  serverAddress: string = 'http://localhost:3000/api';
 
   constructor(private http: Http){}
 
