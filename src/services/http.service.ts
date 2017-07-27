39a577868cb600d1bfe9a88580e35375fe4c646d
Amin Azarbadegan <amin@azarbadegan.net>
2017-07-27 15:33:51 +0430

    Pointing to the prod server, settings.xml to be real

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -14,7 +14,7 @@ export class HttpService{
   // serverAddress: string = isDevMode()?'http://192.168.1.10:3000/api':
   //    'https://quran-together.herokuapp.com/api';
 
-  serverAddress: string = 'http://192.168.15.6:3000/api';
+  serverAddress: string = 'http://read.quran.parts/api';
   isDisconnected: boolean = false;
   user: any = null;
 
