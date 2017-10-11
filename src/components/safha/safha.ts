39996929160a387f3ffb2ae2a442a19602bd3643
Amin Azarbadegan <amin@azarbadegan.net>
2017-10-11 18:38:11 +0330

    Revert "fix some bugs in selecting suras from drop-down list and do not change correct sura name in top of page"
    This reverts commit ecc6b0a0e348324d256fc85aab3d2f7aa42a440a.

--- src/components/safha/safha.ts
+++ src/components/safha/safha.ts
@@ -289,6 +289,7 @@ export class Safha implements OnInit, AfterViewInit, AfterViewChecked {
           this.selectedPages.forEach(page => {
             let ayas = this.quranService.applySectionFilter('page', this.ayas, page);
             this.pageAyas[page] = ayas;
+            this.suraNumbers = this.pageAyas[this._pages[this._pageIndex]].map(e => e.sura).filter((e, i, v) => v.indexOf(e) === i);
             this.repeat[page] = this.repeat[page] ? this.repeat[page] + 1 : 1;
             this.repeatIndex.push(this.repeat[page]);
           });
@@ -335,7 +336,6 @@ export class Safha implements OnInit, AfterViewInit, AfterViewChecked {
       setTimeout(()=>this.calcPagesMetadata(),100);
     }
     else {
-      this.suraNumbers = this.pageAyas[this._pages[this._pageIndex]].map(e => e.sura).filter((e, i, v) => v.indexOf(e) === i);
       let suras = this.suraNumbers.map(e => this.quranService.getSura(e));
       let suraNames = suras.map(e => e.name);
       let suraName = (this.pageAyas[this._pages[this._pageIndex]][0].bismillah === true) ? suraNames[0] : (suraNames[1] ? suraNames[1] : suraNames[0]);
